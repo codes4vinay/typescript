7761bc636db2a822aab39bf748b5431a1b55eb89
Vinay Kumar <149244101+codes4vinay@users.noreply.github.com>
2026-03-23 05:22:02 +0000

    types-union-ts

---
+++ src/typesTS.ts
@@ -0,0 +1,10 @@
+let drink = "chai"
+
+//default datatypes ko appne aap samajh jata h usse type inferencing bolte hai
+
+//type annotation
+let chaiFlavour: string = "masala chai"
+chaiFlavour ="Ginger tea"
+//chaiFlavour = 5 //error not assignable
+
+let chaiOrders: number = 5
