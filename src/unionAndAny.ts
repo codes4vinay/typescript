35ec820c6fc9f6a15e25f557edcceac81ca51619
Vinay Kumar <149244101+codes4vinay@users.noreply.github.com>
2026-03-23 05:21:42 +0000

    types-union- ts

---
+++ src/unionAndAny.ts
@@ -0,0 +1,9 @@
+//union is used to give more than 1 type
+
+let subs: number | string = "1M"
+
+//usage of union
+
+let apiReqStatus: 'pending' | 'success' | 'error' = 'pending'
+
+// apiReqStatus = 'vinay' //error
